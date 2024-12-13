bbd8d0b3cb430c24994972eda6113c323f4e7421
Adrian Ferreiro <adriferreiro.88@gmail.com>
2024-12-13 11:54:53 -0300

    HumanFormats - Números cortos

--- lib/presentation/widgets/movies/movie_horizontal_listview.dart
+++ lib/presentation/widgets/movies/movie_horizontal_listview.dart
@@ -1,4 +1,5 @@
 import 'package:animate_do/animate_do.dart';
+import 'package:cinemapedia/config/helpers/human_formats.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
 
@@ -87,27 +88,27 @@ class _Slide extends StatelessWidget {
               style: textStyles.titleSmall,
             ),
           ),
-          Row(
-            children: [
-              Icon(
-                Icons.star_half_outlined,
-                color: Colors.yellow.shade800,
-              ),
-              const SizedBox(width: 3),
-              Text(
-                '${movie.voteAverage}',
-                style: textStyles.bodyMedium?.copyWith(
+          SizedBox(
+            width: 150,
+            child: Row(
+              children: [
+                Icon(
+                  Icons.star_half_outlined,
                   color: Colors.yellow.shade800,
                 ),
-              ),
-              const SizedBox(
-                width: 10,
-              ),
-              Text(
-                '${movie.popularity}',
-                style: textStyles.bodySmall,
-              ),
-            ],
+                const SizedBox(width: 3),
+                Text(
+                  '${movie.voteAverage}',
+                  style: textStyles.bodyMedium?.copyWith(
+                    color: Colors.yellow.shade800,
+                  ),
+                ),
+                const SizedBox(width: 10),
+                const Spacer(),
+                Text(HumanFormats.number(movie.popularity),
+                    style: textStyles.bodySmall),
+              ],
+            ),
           )
         ],
       ),
