88607b96b8a0ecdc6f0c180b70cc58b29c6892dd
Adrian Ferreiro <adriferreiro.88@gmail.com>
2024-12-16 07:59:19 -0300

    Evitar peticiones repetitivas

--- lib/presentation/providers/movies/movies_providers.dart
+++ lib/presentation/providers/movies/movies_providers.dart
@@ -12,6 +12,7 @@ typedef MovieCallback = Future<List<Movie>> Function({int page});
 
 class MoviesNotifier extends StateNotifier<List<Movie>> {
   int currentPage = 0;
+  bool isloading = false;
   MovieCallback fetchMoreMovies;
 
   //estado inicial es un arreglo vacío porque no tengo ninguna movie al inicio
@@ -20,9 +21,13 @@ class MoviesNotifier extends StateNotifier<List<Movie>> {
   }) : super([]);
 
   Future<void> loadNextPage() async {
+    if (isloading) return;
+    isloading = true;
     //su objetivo es hacerle una modificación al state
     currentPage++;
     final List<Movie> movies = await fetchMoreMovies(page: currentPage);
     state = [...state, ...movies];
+    await Future.delayed(const Duration(milliseconds: 300));
+    isloading = false;
   }
 }
