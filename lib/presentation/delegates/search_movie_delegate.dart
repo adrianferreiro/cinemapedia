9bc14606c40df559e2c04b63309c7b5c2f467624
Adrian Ferreiro <adriferreiro.88@gmail.com>
2025-01-08 22:35:55 -0300

    Sección 15 - cap 229 - SearchDelegate: Leading y Actions

--- lib/presentation/delegates/search_movie_delegate.dart
+++ lib/presentation/delegates/search_movie_delegate.dart
@@ -1,19 +1,40 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
 
-class SearchMovieDelegate extends SearchDelegate {
+class SearchMovieDelegate extends SearchDelegate<Movie?> {
   @override
   String get searchFieldLabel => 'Buscar película';
 
   @override
   List<Widget>? buildActions(BuildContext context) {
     return [
-      const Text('buildActions'),
+      // if (query.isNotEmpty)
+      FadeIn(
+        animate: query.isNotEmpty,
+        duration: const Duration(milliseconds: 200),
+        child: IconButton(
+          onPressed: () =>
+              query = '', //borra lo que se escribe en la caja de texto
+          icon: const Icon(
+            Icons.clear,
+          ),
+        ),
+      )
     ];
   }
 
   @override
   Widget? buildLeading(BuildContext context) {
-    return const Text('buildLeading');
+    return IconButton(
+      onPressed: () => close(
+        context,
+        null, //null porque no se buscó ninguna película
+      ),
+      icon: const Icon(
+        Icons.arrow_back,
+      ),
+    );
   }
 
   @override
